a5a6a8a9ac965802cfd713cfc48ceb0ea9f41437
1halfplusminus <gael.sautron@si2p.nc>
2021-01-08 18:15:05 +1100

    Fetch data with queries

---
+++ start/client/src/index.tsx
@@ -0,0 +1,24 @@
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+} from "@apollo/client";
+import { cache } from "./cache";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectsStyles from "./styles";
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: "http://localhost:4000/graphql",
+});
+
+injectsStyles();
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
