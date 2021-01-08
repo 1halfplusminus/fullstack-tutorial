74db668202cdb514367f6d488b6d9f1a3a718cfc
1halfplusminus <gael.sautron@si2p.nc>
2021-01-08 19:57:02 +1100

    Feat - Update data with mutations

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
+
+import LoginFrom from "./../components/login-form";
+import Loading from "./../components/loading";
+
+import * as LoginTypes from "./__generated__/login";
+import LoginForm from "./../components/login-form";
+
+export const LOGIN_USER = gql`
+  mutation Login($email: String!) {
+    login(email: $email) {
+      id
+      email
+    }
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { loading, error }] = useMutation<
+    LoginTypes.Login,
+    LoginTypes.LoginVariables
+  >(LOGIN_USER, {
+    onCompleted: ({ login }) => {
+      if (login) {
+        localStorage.setItem("token", login.token as string);
+        localStorage.setItem("userId", login.id as string);
+      }
+    },
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>An error occured</p>;
+
+  return <LoginForm login={login} />;
 }
